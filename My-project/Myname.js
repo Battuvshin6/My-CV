477fca9120093e7b1a57fa7f772fc69a01d145dd
Battuvshin6 <mglbaagii03@gmail.com>
2022-05-04 00:30:59 +0800

    [ADD] done i think

--- My-project/Myname.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-app.get("/name", (req, res) => {
-  if (req.url === "/name") {
-    return;
-  }
-});
---
+++ back-end/index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+app.get("/name", (req, res) => {
+  fs.readFile("profile.json", "utf-8", (error, data) => {
+    let myData = JSON.parse(data);
+    let myName = myData.map((e) => {
+      return e.name;
+    });
+    let str = JSON.stringify(myName);
+    console.log(typeof str);
+    if (error) {
+      throw error;
+    } else {
+      return res.send("Hi my name is " + str);
+    }
+  });
+});
+app.get("/age", (req, res) => {
+  fs.readFile("profile.json", "utf-8", (error, data) => {
+    let myData = JSON.parse(data);
+    let myAge = myData.map((e) => {
+      return e.age;
+    });
+    let number = JSON.stringify(myAge);
+
+    console.log(typeof number);
+    if (error) {
+      throw error;
+    } else {
+      return res.send(`I am ${number} years old.`);
+    }
+  });
+});
+app.get("/major", (req, res) => {
+  fs.readFile("profile.json", "utf-8", (error, data) => {
+    let myData = JSON.parse(data);
+    let myMajor = myData.map((e) => {
+      return e.major;
+    });
+    let major = JSON.stringify(myMajor);
+    if (error) {
+      throw error;
+    } else {
+      return res.send(`I am currently a ${major}. `);
+    }
+  });
+});
+app
+  .get("/description", (req, res) => {
+    fs.readFile("profile.json", "utf-8", (error, data) => {
+      let myData1 = JSON.parse(data);
+      let myDescription = myData1.map((e) => {
+        return e.description;
+      });
+      let desc = JSON.stringify(myDescription);
+      if (error) {
+        throw error;
+      } else {
+        return res.send(`1 fact about me is a ${desc}. `);
+      }
+    });
+  })
+  .listen(3001);
+console.log("Started");
